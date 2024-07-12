98ea71744371cd2dd71c3cfaa8e96a85ec461335
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-07-12 16:48:00 +0800

    Code Combine Part 1

---
+++ roll_call_machine_project.ino
@@ -0,0 +1,36 @@
+#include <OneWire.h> 
+#include <DallasTemperature.h> 
+
+#define tempSensorPin 2
+#define irSensorPin 3
+
+OneWire oneWire ( tempSensorPin );
+DallasTemperature sensors ( &oneWire );
+
+void setup ()
+{
+  Serial.begin ( 115200 );
+
+  pinMode ( irSensorPin, INPUT_PULLUP );
+  sensors.begin ();
+}
+
+void loop ()
+{
+    int L = digitalRead ( irSensorPin );
+  
+    if ( L == 0 )
+    {
+        Serial.print ( " Obstacle detected    " );
+        sensors.requestTemperatures ();
+        Serial.println ( sensors.getTempCByIndex ( 0 ) );
+    
+    }
+    
+    else
+    {
+        Serial.println ( "  === All clear" );
+    }
+
+    delay(100);
+}

9340e8c591fa921f36f5c5d1929ee9ca1a0c5d2f
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-07-15 18:03:07 +0800

    Code Combine Part 2
    - Combine old code to ESP-32 (Line Notify.)

--- roll_call_machine_project.ino
+++ roll_call_machine_project.ino
@@ -1,8 +1,15 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <TridentTD_LineNotify.h>
 #include <OneWire.h> 
 #include <DallasTemperature.h> 
 
 #define tempSensorPin 3
 #define irSensorPin 2
+#define LINE_TOKEN "Y3nL5gLv1Q7UiDshiv2rPZAXc4jbouEqzt04HmilnZo"
+
+const char* ssid     = "FuNet-UAP";
+const char* password = "25785657";
 
 OneWire oneWire ( tempSensorPin );
 DallasTemperature sensors ( &oneWire );
@@ -13,6 +20,9 @@ void setup ()
 
   pinMode ( irSensorPin, INPUT_PULLUP );
   sensors.begin ();
+
+  wifi_setup ();
+  line_setup ();
 }
 
 void loop ()
@@ -21,7 +31,7 @@ void loop ()
   
     if ( L == 0 )
     {
-        Serial.print ( " Obstacle detected    " );
+        LINE.notify ( "Obstacle detected    " );
         sensors.requestTemperatures ();
         Serial.println ( sensors.getTempCByIndex ( 0 ) );
     
@@ -29,8 +39,32 @@ void loop ()
     
     else
     {
-        Serial.println ( "  === All clear" );
+        LINE.notify ( "=== All clear" );
     }
 
     delay(100);
+}
+
+void wifi_setup ()
+{
+    WiFi.mode ( WIFI_STA );
+    WiFi.begin ( ssid, password );
+
+    while ( WiFi.status () != WL_CONNECTED )
+    {
+      Serial.print ( "." );
+      delay ( 500 );
+    }
+
+    Serial.println ( "" );
+    Serial.print ( "Connected to " );
+    Serial.println ( ssid );
+    Serial.print ( "IP address: " );
+    Serial.println ( WiFi.localIP () );
+}
+
+void line_setup ()
+{
+    LINE.setToken ( LINE_TOKEN );
+    LINE.notify ( "Line Notify Link Confirm." );
 }

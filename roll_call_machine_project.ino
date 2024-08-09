2cbb34c845ff33d07a8c66fa28b18d0c602423c7
Quodlun <quodlunofficial@gmail.com>
2024-08-09 12:14:59 +0800

    Pin Number Changed

--- roll_call_machine_project.ino
+++ roll_call_machine_project.ino
@@ -4,12 +4,12 @@
 #include <OneWire.h> 
 #include <DallasTemperature.h> 
 
-#define tempSensorPin 3
 #define irSensorPin 2
+#define tempSensorPin 4
 #define LINE_TOKEN "Y3nL5gLv1Q7UiDshiv2rPZAXc4jbouEqzt04HmilnZo"
 
-const char* ssid     = "FuNet-UAP";
-const char* password = "25785657";
+const char* ssid     = "When Can My Internet Get Better";
+const char* password = "O00O00O0";
 
 OneWire oneWire ( tempSensorPin );
 DallasTemperature sensors ( &oneWire );
@@ -68,3 +68,31 @@ void line_setup ()
     LINE.setToken ( LINE_TOKEN );
     LINE.notify ( "Line Notify Link Confirm." );
 }
+
+/*
+Sketch 使用 1008329 位元組（76%）的程式儲存空間。最大為 1310720 位元組
+全域變數使用 45364 位元組 (13%) 的動態記憶體, 保留 282316 位元組給區域變數. 最大 327680 位元組
+esptool.py v4.6
+Serial port COM8
+Connecting.........Traceback (most recent call last):
+  File "esptool.py", line 37, in <module>
+  File "esptool\__init__.py", line 1064, in _main
+  File "esptool\__init__.py", line 859, in main
+  File "esptool\cmds.py", line 466, in write_flash
+  File "esptool\util.py", line 37, in flash_size_bytes
+TypeError: argument of type 'NoneType' is not iterable
+
+Chip is ESP32-D0WD-V3 (revision v3.1)
+Features: WiFi, BT, Dual Core, 240MHz, VRef calibration in efuse, Coding Scheme None
+Crystal is 40MHz
+MAC: ec:64:c9:5d:60:60
+Uploading stub...
+Running stub...
+Stub running...
+Changing baud rate to 921600
+Changed.
+WARNING: Failed to communicate with the flash chip, read/write operations will fail. Try checking the chip connections or removing any other hardware connected to IOs.
+Configuring flash size...
+[10508] Failed to execute script 'esptool' due to unhandled exception!
+上傳失敗: 上傳錯誤: exit status 1
+*/

95280d4180f90bb3d56a97ed15332b740043465b
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-11-06 17:46:48 +0800

    Debugger Added

--- roll_call_machine_project_copied/roll_call_machine_project_copied.ino
+++ roll_call_machine_project_copied/roll_call_machine_project_copied.ino
@@ -36,7 +36,15 @@ void setup()
 
 void loop()
 {
-  int L = digitalRead(irSensorPin);
+  /*
+    Fill the code of the thing you want to debug, blank or 1 if you don't want any.
+    1: Wi-Fi
+    2: IR Sensor
+    3: Line Notify
+    4: LCD
+    5: Bumper
+  */
+  debug ();
 
   if (digitalRead(irSensorPin) == LOW)
   {
@@ -62,11 +70,15 @@ void loop()
 
 void wifiSetup()
 {
+  int offline_count = 0;
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
   {
+    offline_count ++;
+    Serial.println ( f"Debug: WiFi Setup Failed (%d)", offline_count );
     delay(500);
   }
 }
@@ -154,4 +166,107 @@ void bumperWork()
 void tempSensorGet()
 {
   objectTemp = sensor.getObjectTempCelsius();
+}
+
+// Debug Code
+void debug ( debug_code );
+{
+  switch ( debug_code )
+  {
+    case 1:
+      wifiDebug ();
+      break;
+
+    case 2:
+      irSensorDebug ();
+      break;
+
+    case 3:
+      lineNotifyDebug ();
+      break;
+
+    case 4:
+      lcdDebug ();
+      break;
+
+    case 5:
+      bumperDebug ();
+      break;
+  
+    default:
+      break;
+  }
+}
+void irSensorDebug ()
+{
+  if ( digitalRead ( irSensorPin ) == LOW )
+  {
+    Serial.println ( "Object Detected." );
+  }
+
+  else
+  {
+    Serial.println ( "IR Sensor Clear" );
+  }
+
+  delay ( 1000 );
+}
+
+void lineNotifyDebug ()
+{
+  LINE.notify ( "Debug: Line Notify Okay." );
+
+  delay ( 1000 );
+}
+
+void wifiDebug ()
+{
+  int offline_count = 0;
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    offline_count ++;
+    Serial.println ( f"Debug: WiFi Dropped (%d)"7, offline_count );
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+
+    delay(500);
+  }
+}
+
+void lcdDebug ()
+{
+  lcd.clear ();
+
+  for ( int i = 0; i < 3, i ++ )
+  {
+    lcd.setCursor ( 0, 0 );
+    lcd.print ( "LCD Debug Mode" );
+    delay ( 1000 );
+    
+    lcd.clear ();
+    delay ( 1000 );
+  }
+
+  while ( true )
+  {
+    lcd.setCursor ( 0, 0 );
+    lcd.print ( "0123456789abcdef" );
+    lcd.setCursor ( 0, 1 );
+    lcd.print ( "0123456789abcdef" );
+    delay ( 5000 );
+    
+    lcd.clear ();
+    delay ( 1000 );
+  }
+}
+
+void bumperDebug ();
+{
+  digitalWrite ( bumperPin, HIGH );
+  delay ( 5000 );
+
+  digitalWrite ( bumperPin, LOW );
+  delay ( 5000 );
 }

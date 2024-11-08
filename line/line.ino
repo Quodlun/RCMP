24b34ad8669175e90e585ceb80840e22502fcc9f
shrimp-blind <jlin82077@gmail.com>
2024-11-08 16:06:42 +0800

    file

--- line/line.ino
+++ line/line.ino
@@ -1,32 +1,13 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <TridentTD_LineNotify.h>
-#include <OneWire.h> 
-#include <DallasTemperature.h> 
+//紅色接地 黑色接17 黃16 綠and白接5V 型號 HLK-ZW0632 接腳要改自己改
 #include <CapacitiveFingerprint.h>  // 假設電容式指紋傳感器庫
 
-// Pin Definitions
-#define LINE_TOKEN "MbcvZH9nf1mvsSkXBzEpoQDQNOaNkWpHJ2nmoSBSBpz"
-
-// WiFi Credentials
-const char* ssid     = "When Can My Internet Get Better";
-const char* password = "O00O00O0";
-
-
-
-// 假設電容式指紋傳感器的設置
+// 指紋傳感器設置
 HardwareSerial mySerial(2); 
 CapacitiveFingerprint finger(&mySerial);  // 使用電容式指紋模組的庫
 
 void setup() {
   Serial.begin(115200);
-
-  // Initialize Sensors
-  sensors.begin();
-  wifi_setup();
-  line_setup();
   fingerprint_setup();  // 初始化指紋傳感器
-
   Serial.println("System Ready");
 }
 
@@ -36,34 +17,10 @@ void loop() {
   if (fingerprintID >= 0) {
     Serial.print("識別到指紋，ID: ");
     Serial.println(fingerprintID);
-    LINE.notify("Fingerprint ID: " + String(fingerprintID));
   } else {
     Serial.println("No Match Found");
-    LINE.notify("警告!有未知人士正在使用點名器");
   }
-
   delay(1000); // 每秒檢查一次
-}
-
-// WiFi 設定
-void wifi_setup() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.println("\nConnected to WiFi");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-// Line Notify 設定
-void line_setup() {
-  LINE.setToken(LINE_TOKEN);
-  LINE.notify("Line Notify Link Confirm.");
 }
 
 // 指紋傳感器初始化
@@ -85,10 +42,10 @@ int getFingerprintID() {
   uint8_t p = finger.getImage();
   
   // 檢查是否檢測到手指
-  if (p == CAPFINGERPRINT_NOFINGER) {  // 修改為電容式指紋的狀態常量
+  if (p == FINGERPRINT_NOFINGER) {  // 正確的指紋狀態常量
     Serial.println("未檢測到手指");
     return -1;
-  } else if (p == CAPFINGERPRINT_OK) {  // 電容式指紋的成功狀態
+  } else if (p == FINGERPRINT_OK) {  // 成功狀態
     Serial.println("指紋檢測成功");
   } else {
     Serial.println("指紋檢測失敗");
@@ -97,7 +54,7 @@ int getFingerprintID() {
 
   // 搜尋指紋匹配
   p = finger.fingerSearch();
-  if (p == CAPFINGERPRINT_OK) {  // 電容式匹配成功的狀態
+  if (p == FINGERPRINT_OK) {  // 匹配成功
     Serial.print("找到匹配的指紋，ID #"); 
     Serial.println(finger.fingerID);
     return finger.fingerID;
@@ -105,6 +62,4 @@ int getFingerprintID() {
     Serial.println("未找到匹配的指紋");
     return -1;
   }
-}
-
 }

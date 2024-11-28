8ed856d61da18b3569476f17a6df2917a6d41c01
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-11-28 23:23:37 +0800

    Wi-Fi Test Code

---
+++ wifi_test/wifi_test.ino
@@ -0,0 +1,25 @@
+#include <WiFi.h>
+#include "ExternVariable.h"
+
+void setup ()
+{
+  Serial.begin ( 115200 );
+  WiFi begin ( ssid, password );
+  
+  while ( WiFi.status () != WL_CONNECTED )
+  {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  Serial.print ( "\n IP: " );
+  Serial.println ( "WiFi.localIP () );
+
+  Serial,println ( "WiFi status:" );
+  WiFi.printDiag ( Serial );
+}
+
+void void loop ()
+{
+  delay ( 1000 );
+}

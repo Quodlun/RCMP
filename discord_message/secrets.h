bca8042201ff4b2e440892bf146871c0d159fe22
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-12-03 21:42:41 +0800

    File Sorted

--- discord_message/secrets.h
+++
@@ -1,4 +0,0 @@
-#define WIFI_SSID "FuNet-UAP"
-#define WIFI_PASS "25785657"
-#define DISCORD_WEBHOOK "https://discordapp.com/api/webhooks/1313348368633827338/3n9_-o7_LR6bJjSLt-h638emCgcHlqqUb2GjnR5IK9rdPX_ATiet35Mcmq_60YVWFH9N"
-#define DISCORD_TTS "false"
--- discord_test/discord_test_mkr1010/arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID ""
-#define SECRET_PASS ""
-//Copy the webhook url (without https://discordapp) here:
-#define SECRET_WEBHOOK "/api/webhooks/..."
-#define SECRET_TTS ""

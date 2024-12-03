3dc095a89d9bf87d5111f5a0822f94b43699dda4
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-12-03 15:40:42 +0800

    Test Code Changed

--- discord_message/secrets.h
+++ discord_message/secrets.h
@@ -1,4 +1,4 @@
-#define WIFI_SSID "When Can My Internet Get Better"
-#define WIFI_PASS "zj4joxu3"
+#define WIFI_SSID "FuNet-UAP"
+#define WIFI_PASS "25785657"
 #define DISCORD_WEBHOOK "https://discordapp.com/api/webhooks/1313348368633827338/3n9_-o7_LR6bJjSLt-h638emCgcHlqqUb2GjnR5IK9rdPX_ATiet35Mcmq_60YVWFH9N"
 #define DISCORD_TTS "false"

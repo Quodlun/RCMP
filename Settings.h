c699cb0550b0dd878998140cae521f6ead0ef5c7
Saiki <115924991+Quodlun@users.noreply.github.com>
2024-12-31 14:16:24 +0800

    Update Settings.h

--- Settings.h
+++ Settings.h
@@ -2,9 +2,9 @@
 #define SETTINGS_H
 
 // Wi-Fi & WebHook Setting
-extern const char* discordWebhook = "CONFIDENTIAL";
-extern const char* ssid = "CONFIDENTIAL";
-extern const char* password = "CONFIDENTIAL";
+extern const char* discordWebhook = "https://discordapp.com/api/webhooks/1313348368633827338/3n9_-o7_LR6bJjSLt-h638emCgcHlqqUb2GjnR5IK9rdPX_ATiet35Mcmq_60YVWFH9N";
+extern const char* ssid = "When Can My Internet Get Better";
+extern const char* password = "zj4joxu3";
 
 // NTP Time Setting
 extern const char* ntpServer = "time.google.com";
